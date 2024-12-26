d5b7a2e3c96b7910935295f992431f6a98eb28d6
Le Van Toan <levantoan01@gmail.com>
2024-12-26 22:02:52 +0700

    video 28

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const MyComponent = () => {
+  return (
+    <div>Lê Văn Toản V1.0</div>
+  );
+}
+
+const App = () => {
   const [count, setCount] = useState(0)
+
+
 
   return (
     <>
@@ -17,6 +25,7 @@ function App() {
         </a>
       </div>
       <h1>Lê Văn Toản</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
